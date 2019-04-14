d96c2010d8002637444ac826e0969bdee6efa7a3
John Diehl <jrd1250@gmail.com>
2019-04-14 15:32:51 -0500

    skeleton implementation of DBXMLFFunctions class

--- BaylorBlackGospelVR/Source/BaylorBlackGospelVR/Private/DBXMLFFunctions.cpp
+++ BaylorBlackGospelVR/Source/BaylorBlackGospelVR/Private/DBXMLFFunctions.cpp
@@ -1,5 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include <vector>
+
 #include "DBXMLFFunctions.h"
 
 DBXMLFFunctions::DBXMLFFunctions()
@@ -8,4 +10,16 @@ DBXMLFFunctions::DBXMLFFunctions()
 
 DBXMLFFunctions::~DBXMLFFunctions()
 {
+}
+
+vector<pair<int, string>> DBXMLFFunctions::getSearchableRecordList(string xml) {
+	return new vector<pair<int, string>>();
+}
+
+RecordData DBXMLFFunctions::getRecordData(string xml) {
+	return new RecordData();
+}
+
+void DBXMLFFunctions::populateSongAndImageFiles(RecordData& recordData, string xml) {
+
 }
