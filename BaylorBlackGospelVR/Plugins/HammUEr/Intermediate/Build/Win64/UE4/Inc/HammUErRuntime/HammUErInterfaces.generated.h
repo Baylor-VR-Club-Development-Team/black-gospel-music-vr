a581f716f6a1e4f446a98770cd0f017d31e85f5b
Sean Blonien <sean_blonien@baylor.edu>
2019-07-02 21:29:34 -0500

    Added working demo C++ class, with required build/source files

---
+++ BaylorBlackGospelVR/Plugins/HammUEr/Intermediate/Build/Win64/UE4/Inc/HammUErRuntime/HammUErInterfaces.generated.h
@@ -0,0 +1,109 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef HAMMUERRUNTIME_HammUErInterfaces_generated_h
+#error "HammUErInterfaces.generated.h already included, missing '#pragma once' in HammUErInterfaces.h"
+#endif
+#define HAMMUERRUNTIME_HammUErInterfaces_generated_h
+
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_RPC_WRAPPERS
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_RPC_WRAPPERS_NO_PURE_DECLS
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	HAMMUERRUNTIME_API UHammuerLogicInterface(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UHammuerLogicInterface) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(HAMMUERRUNTIME_API, UHammuerLogicInterface); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UHammuerLogicInterface); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	HAMMUERRUNTIME_API UHammuerLogicInterface(UHammuerLogicInterface&&); \
+	HAMMUERRUNTIME_API UHammuerLogicInterface(const UHammuerLogicInterface&); \
+public:
+
+
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	HAMMUERRUNTIME_API UHammuerLogicInterface(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	HAMMUERRUNTIME_API UHammuerLogicInterface(UHammuerLogicInterface&&); \
+	HAMMUERRUNTIME_API UHammuerLogicInterface(const UHammuerLogicInterface&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(HAMMUERRUNTIME_API, UHammuerLogicInterface); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UHammuerLogicInterface); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UHammuerLogicInterface)
+
+
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_GENERATED_UINTERFACE_BODY() \
+private: \
+	static void StaticRegisterNativesUHammuerLogicInterface(); \
+	friend struct Z_Construct_UClass_UHammuerLogicInterface_Statics; \
+public: \
+	DECLARE_CLASS(UHammuerLogicInterface, UInterface, COMPILED_IN_FLAGS(CLASS_Abstract | CLASS_Interface), CASTCLASS_None, TEXT("/Script/HammUErRuntime"), HAMMUERRUNTIME_API) \
+	DECLARE_SERIALIZER(UHammuerLogicInterface)
+
+
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_GENERATED_BODY_LEGACY \
+		PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+	BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_GENERATED_UINTERFACE_BODY() \
+	BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_STANDARD_CONSTRUCTORS \
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_GENERATED_BODY \
+	PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+	BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_GENERATED_UINTERFACE_BODY() \
+	BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_ENHANCED_CONSTRUCTORS \
+private: \
+	PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_INCLASS_IINTERFACE_NO_PURE_DECLS \
+protected: \
+	virtual ~IHammuerLogicInterface() {} \
+public: \
+	typedef UHammuerLogicInterface UClassType; \
+	typedef IHammuerLogicInterface ThisClass; \
+	virtual UObject* _getUObject() const { check(0 && "Missing required implementation."); return nullptr; }
+
+
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_INCLASS_IINTERFACE \
+protected: \
+	virtual ~IHammuerLogicInterface() {} \
+public: \
+	typedef UHammuerLogicInterface UClassType; \
+	typedef IHammuerLogicInterface ThisClass; \
+	virtual UObject* _getUObject() const { check(0 && "Missing required implementation."); return nullptr; }
+
+
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_5_PROLOG
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_RPC_WRAPPERS \
+	BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_INCLASS_IINTERFACE \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_RPC_WRAPPERS_NO_PURE_DECLS \
+	BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h_8_INCLASS_IINTERFACE_NO_PURE_DECLS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BaylorBlackGospelVR_Plugins_HammUEr_Source_HammUErRuntime_Public_HammUErInterfaces_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
